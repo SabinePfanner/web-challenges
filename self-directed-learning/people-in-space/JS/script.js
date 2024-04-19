a704798228be8d28973e4ee0085996e5f0d7474d
Sabine Pfanner <pfanner.sabine@gmail.com>
2024-04-19 15:56:02 +0200

    add challenges of 19.04 2

--- self-directed-learning/people-in-space/JS/script.js
+++ self-directed-learning/people-in-space/JS/script.js
@@ -7,10 +7,11 @@ async function fetchData() {
   const response = await fetch(url);
   const data = await response.json();
   numberPeopleInSpace.textContent = data.number;
-  //   console.log(data);
-  //   console.log(data.people);
-  //   console.log(data.people[0].name);
+  // console.log(data);
+  // console.log(data.people);
+  // console.log(data.people[0].name);
   renderPeople(data.people);
+  renderCrafts(data.people);
 }
 
 fetchData();
@@ -24,4 +25,16 @@ function renderPeople(people) {
     listElement.textContent = person.name;
     ul.append(listElement);
   });
+}
+
+function renderCrafts(crafts) {
+  crafts.forEach((person) => {
+    const button = document.createElement("button");
+    button.textContent = person.craft;
+    ul.append(button);
+  });
+}
+
+button.addEventListener("click" => {
+  const craftTeam = people.filter ((person)) => person.craft = "ISS");
 }
