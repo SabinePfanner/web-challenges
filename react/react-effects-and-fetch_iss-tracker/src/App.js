6f9c0bf0cb5b722c901c0187d806661b92c33ce5
Sabine Pfanner <pfanner.sabine@gmail.com>
2024-04-29 14:21:22 +0200

    challenges 29.04. morning session

---
+++ react/react-effects-and-fetch_iss-tracker/src/App.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import Controls from "./components/Controls";
+import Map from "./components/Map";
+import "./styles.css";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export default function App() {
+  const [coords, setCoords] = useState({
+    longitude: 0,
+    latitude: 0,
+  });
+
+  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setCoords(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const intervalID = setInterval(getISSCoords, 5000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  if (!coords) {
+    return <>loading...</>;
+  }
+
+  return (
+    <main>
+      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Controls
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+        onRefresh={getISSCoords}
+      />
+    </main>
+  );
+}
